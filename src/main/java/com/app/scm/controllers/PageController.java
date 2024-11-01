49d3047487c3acaa8ac87a712b1e8ee69d1c4cc2
Deepanshu Aggarwal <deepanshu.26.da@gmail.com>
2024-11-01 18:02:13 +0530

    understanding fragments

--- src/main/java/com/app/scm/controllers/PageController.java
+++ src/main/java/com/app/scm/controllers/PageController.java
@@ -1,15 +1,27 @@
 package com.app.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home() {
         System.out.println("home page handler!!");
         return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("isLogin", true);
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String services() {
+        return "services";
     }
 
 }
